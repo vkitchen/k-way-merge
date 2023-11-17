e9b9f17fdf6a94337cdc2dae68fe84841de0180e
Vaughan Kitchen <contact@tophat.nz>
2023-11-17 14:37:18 +1300

    Support other values of n, insertion sort indexes

--- merge_insertion_sort_indexes.cpp
+++ merge_insertion_sort_indexes.cpp
@@ -3,9 +3,9 @@
 static int *postings[16];
 static unsigned char segments[16] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
 
-static void sort_full() {
+static void sort_full(int length) {
 	int i, j;
-	for (i = 1; i < 16; i++) {
+	for (i = 1; i < length; i++) {
 		unsigned char x = segments[i];
 		for (j = i - 1; j >= 0 && *postings[segments[j]] < *postings[x]; j--)
 			segments[j+1] = segments[j];
@@ -13,22 +13,19 @@ static void sort_full() {
 	}
 }
 
-static void sort_partial() {
+static void sort_partial(int length) {
 	int i;
 	unsigned char x = segments[0];
-	for (i = 0; i < 15 && *postings[segments[i+1]] > *postings[x]; i++)
+	for (i = 0; i < length-1 && *postings[segments[i+1]] > *postings[x]; i++)
 		segments[i] = segments[i+1];
 	segments[i] = x;
 }
 
 void merge_insertion_sort_indexes(struct test *t, int n) {
-	if (n != 16)
-		return;
-
-	for (int i = 0; i < 16; i++)
+	for (int i = 0; i < n; i++)
 		postings[i] = t->postings[i];
 
-	sort_full();
+	sort_full(n);
 
 	// process
 	size_t pos = 0;
@@ -38,6 +35,6 @@ void merge_insertion_sort_indexes(struct test *t, int n) {
 
 		t->results[pos++] = *postings[segments[0]]++;
 
-		sort_partial();
+		sort_partial(n);
 	}
 }
