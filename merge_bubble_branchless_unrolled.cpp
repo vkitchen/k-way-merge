647208d343f22fae8a4a718877cd88dbecdbcaf4
Vaughan Kitchen <contact@tophat.nz>
2026-07-24 17:02:48 +1200

    Further unrolling

--- merge_bubble_branchless_unrolled.cpp
+++ merge_bubble_branchless_unrolled.cpp
@@ -79,6 +79,270 @@ bool MergeBubbleBranchlessUnrolled::merge(struct test *t, int n) {
 
 			sort_partial(segments, 6);
 		}
+	} else if (n == 7) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 7);
+		}
+	} else if (n == 8) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 8);
+		}
+	} else if (n == 9) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 9);
+		}
+	} else if (n == 10) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 10);
+		}
+	} else if (n == 11) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 11);
+		}
+	} else if (n == 12) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 12);
+		}
+	} else if (n == 13) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 13);
+		}
+	} else if (n == 14) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 14);
+		}
+	} else if (n == 15) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 15);
+		}
+	} else if (n == 16) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 16);
+		}
+	} else if (n == 17) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 17);
+		}
+	} else if (n == 18) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 18);
+		}
+	} else if (n == 19) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 19);
+		}
+	} else if (n == 20) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 20);
+		}
+	} else if (n == 21) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 21);
+		}
+	} else if (n == 22) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 22);
+		}
+	} else if (n == 23) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 23);
+		}
+	} else if (n == 24) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 24);
+		}
+	} else if (n == 25) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 25);
+		}
+	} else if (n == 26) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 26);
+		}
+	} else if (n == 27) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 27);
+		}
+	} else if (n == 28) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 28);
+		}
+	} else if (n == 29) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 29);
+		}
+	} else if (n == 30) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (*segments[0] == 0)
+				break;
+
+			t->results[pos++] = *segments[0]++;
+
+			sort_partial(segments, 30);
+		}
 	} else {
 		// process
 		size_t pos = 0;
--- merge_bubble_branchless_unrolled_cache.cpp
+++ merge_bubble_branchless_unrolled_cache.cpp
@@ -92,6 +92,294 @@ bool MergeBubbleBranchlessUnrolledCache::merge(struct test *t, int n) {
 
 			sort_partial(segments, cache, 6);
 		}
+	} else if (n == 7) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 7);
+		}
+	} else if (n == 8) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 8);
+		}
+	} else if (n == 9) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 9);
+		}
+	} else if (n == 10) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 10);
+		}
+	} else if (n == 11) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 11);
+		}
+	} else if (n == 12) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 12);
+		}
+	} else if (n == 13) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 13);
+		}
+	} else if (n == 14) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 14);
+		}
+	} else if (n == 15) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 15);
+		}
+	} else if (n == 16) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 16);
+		}
+	} else if (n == 17) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 17);
+		}
+	} else if (n == 18) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 18);
+		}
+	} else if (n == 19) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 19);
+		}
+	} else if (n == 20) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 20);
+		}
+	} else if (n == 21) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 21);
+		}
+	} else if (n == 22) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 22);
+		}
+	} else if (n == 23) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 23);
+		}
+	} else if (n == 24) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 24);
+		}
+	} else if (n == 25) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 25);
+		}
+	} else if (n == 26) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 26);
+		}
+	} else if (n == 27) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 27);
+		}
+	} else if (n == 28) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 28);
+		}
+	} else if (n == 29) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 29);
+		}
+	} else if (n == 30) {
+		// process
+		size_t pos = 0;
+		for (;;) {
+			if (cache[0] == 0)
+				break;
+
+			t->results[pos++] = cache[0];
+			cache[0] = *++segments[0];
+
+			sort_partial(segments, cache, 30);
+		}
 	} else {
 		// process
 		size_t pos = 0;
